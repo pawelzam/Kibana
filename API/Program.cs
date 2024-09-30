2f99806d08d891457fe937e2b86e41eacec65f80
Pawel Zamrij <PZAM@danskebank.dk>
2024-09-30 13:47:53 +0200

    Initial

---
+++ API/Program.cs
@@ -0,0 +1,60 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((hostBuilder, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(config["ElasticSearch:Uri"]!))
+        {
+            IndexFormat = $"{config["AppName"]}-{builder.Environment.EnvironmentName.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+            AutoRegisterTemplate = true,
+            OverwriteTemplate = true,
+            TemplateName = config["AppName"],
+            AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+            TypeName = null,
+            BatchAction = ElasticOpType.Create
+        });
+});
+//builder.Services.AddSerilog(c => 
+//    c.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(config["ElasticSearch:Uri"]!))
+//    {
+//        IndexFormat = $"{config["AppName"]}-logs-{builder.Environment.EnvironmentName.ToLower().Replace(".","-")}-{DateTime.UtcNow:yyyy-MM}",
+//        AutoRegisterTemplate = true,
+//        OverwriteTemplate = true,
+//        TemplateName = "kibana-app",
+//        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+//        TypeName = null,
+//        BatchAction = ElasticOpType.Create
+//    })
+//    .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+//    .ReadFrom.Configuration(config));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+app.MapGet("/get", (ILogger<Program> logger) =>
+{
+    logger.LogInformation("Hello World!");
+})
+.WithName("Get")
+.WithOpenApi();
+
+app.Run();
+
